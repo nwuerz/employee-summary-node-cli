221629ac048e51d39a6c463cba5f93f46888babd
nick wuerz <nawuerz@gmail.com>
2020-03-06 22:08:48 -0600

    fixed bug

--- Develop/app.js
+++ Develop/app.js
@@ -4,15 +4,12 @@ const Intern = require("./lib/Intern");
 const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
-
+const open = require('open');
 const OUTPUT_DIR = path.resolve(__dirname, "output")
 const outputPath = path.join(OUTPUT_DIR, "team.html");
-​
 const render = require("./lib/htmlRenderer");
-​
 const teamMembers = [];
 const idArray = [];
-​
 function appMenu() {
 // Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
@@ -77,7 +74,7 @@ function createTeam() {
 function addEngineer() {
     inquirer.prompt([
         {
-          type: "list",
+          type: "input",
           name: "engineerName",
           message: "What is your engineer's name?"  
         },
@@ -138,26 +135,15 @@ function addIntern() {
 // above) and pass in an array containing all employee objects; the `render` function will
 // generate and return a block of HTML including templated divs for each employee!
 function buildTeam() {
-    fs.writeFileSync(outputPath, render(teamMembers), "utf-8");
+    fs.writeFile(outputPath, render(teamMembers), "utf-8", function(err) {
+        if (err) {
+            throw err;
+        }
+        open(outputPath)
+    });
 }
 
 createManager();
-​
 }
 
 appMenu();
-// After you have your html, you're now ready to create an HTML file using the HTML
-// returned from the `render` function. Now write it to a file named `team.html` in the
-// `output` folder. You can use the variable `outputPath` above target this location.
-// Hint: you may need to check if the `output` folder exists and create it if it
-// does not.
-​
-// HINT: each employee type (manager, engineer, or intern) has slightly different
-// information; write your code to ask different questions via inquirer depending on
-// employee type.
-​
-// HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
-// and Intern classes should all extend from a class named Employee; see the directions
-// for further information. Be sure to test out each class and verify it generates an 
-// object with the correct structure and methods. This structure will be crucial in order
-// for the provided `render` function to work!```
--- Develop/lib/Engineer.js
+++ Develop/lib/Engineer.js
@@ -10,7 +10,7 @@ class Engineer extends Employee {
     getGithub() {
         return this.github;
     }
-
+    //super?
     getRole() {
         return "Engineer";
     }
